7a1f85f5aff52a49c1bad2ceb93dfb9e6abe9a5c
Ben <bigriffin94@outlook.com>
2020-02-20 15:40:17 -0800

    Recipe Tests passing

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,57 +2,67 @@
 
 import math
 
-# recipe & ingredients arguments needs to be a dict
-# dicts need to be in the same form
-
-# what I understand:
-# - values in ingredients must be >= values in the recipe before we check how many batches we can make
-	# - or if keys do not match
-
-# - use a dict method .items() in order to iterate through recipe/ingredients
-# - use our_recipe items in order to compare recipe to ingredients needed
-# - batch variable to track number of possible batches
-
-# - min loop == 1 in order to see if our first item in recipe is <= first item in ingredient
-
 def recipe_batches(recipe, ingredients):
 
-	# batches = {}
-	# num_of_possible_batches = []
-	batches = 0
-	print("Recipe.keys(): ", recipe.keys())
-	print("Ingredients.keys(): ", ingredients.keys())
+	# var for batches arr
+	batches = []
 
-	# loop through recipe
-	for key in recipe.items():
-		print("RK: ", recipe.keys())
-		print("IK: ", ingredients.keys())
+	# iterate over recipe
+	for i in recipe.keys():
 
-		# compare keys in recipe to keys in ingredients and update # of batches
-		if key in ingredients and recipe[key] < ingredients[key]:
-			value = recipe[key] // ingredients[key]
-			batches = value
-			print("batches: ", batches)
-			return batches
+	# if key exist in ingredients and is less than or = to igredients then we have enoughfor 1 batch
+		if i in ingredients and recipe[i] <= ingredients[i]:
+		# save batch count to ingredient batch list
+			batches.append(ingredients[i] // recipe[i])
 
 		else:
-			# no possible batches if we get here
 			return 0
-		
 
-		
+	# find smallest batch number
+	return min(batches)
 
-	# loop trhough batches and do math
-	return "Return statement"
+
+
+if __name__ == '__main__':
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+
+
+
+
+# 1st attempt:
+	# # create arrs to store values to iterate through
+	# recipe_arr = []
+	# ingred_arr = []
+	# batches = 0
 	
+
+
+	# # loop through recipe.items()
+	# for i in recipe.items():
+	# 	print("I am items: ", i[1])
+	# 	recipe_arr.append(i[1])
 	
-print(recipe_batches({ 'milk': 2, 'sugar': 40, 'butter': 20 }, { 'milk': 5, 'sugar': 120, 'butter': 500 }))
+
+	# for j in ingredients.items():
+	# 	print("I am items: ", j[1])
+	# 	ingred_arr.append(j[1])
 
 
-# if __name__ == '__main__':
-#     # Change the entries of these dictionaries to test
-#     # your implementation with different inputs
-#     recipe = {'milk': 100, 'butter': 50, 'flour': 5}
-#     ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
-#     print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
-#         batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+	# print(recipe_arr)
+	# print(ingred_arr)
+
+	# for r, i in list(zip(recipe_arr, ingred_arr)):
+
+	# 	if recipe_arr[r] <= ingred_arr[i]:
+	# 		batches = r
+	# 		print("r", r)
+	# 		print("i", i)
+	# 		print("batches: ", batches)
+	# 	else:
+	# 		print("we can not make a batch")
+
